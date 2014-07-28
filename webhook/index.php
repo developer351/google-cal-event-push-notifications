9b54778695b5ce38ddf3f6d659697308ea97e861
Brice Bentler <server4001@gmail.com>
2014-07-27 20:20:11 -0700

    Cleans up log to file

--- webhook/index.php
+++ webhook/index.php
@@ -1,67 +1,44 @@
 <?php
 
-$headers = getallheaders();
-$data = array();
-$unused_elseifs = 0;
+// @TODO: Add test for token as well...
+if(substr($_SERVER['HTTP_USER_AGENT'], 0, 11) === 'APIs-Google') {
+	$data = array();
 
-$data['http_user_agent'] = $_SERVER['HTTP_USER_AGENT']; // Check for: APIs-Google
+	$data['http_user_agent'] = $_SERVER['HTTP_USER_AGENT']; // Check for: APIs-Google
 
-$data['boolean'] = (substr($_SERVER['HTTP_USER_AGENT'], 0, 11) === 'APIs-Google') ? 'true' : 'false';
+	if(isset($_SERVER['HTTP_X_GOOG_CHANNEL_ID'])) {
+		$data['x_goog_channel_id'] = $_SERVER['HTTP_X_GOOG_CHANNEL_ID'];
+	}
+	if(isset($_SERVER['HTTP_X_GOOG_MESSAGE_NUMBER'])) {
+		$data['x_goog_message_number'] = $_SERVER['HTTP_X_GOOG_MESSAGE_NUMBER'];
+	}
+	if(isset($_SERVER['HTTP_X_GOOG_RESOURCE_ID'])) {
+		$data['x_goog_resource_id'] = $_SERVER['HTTP_X_GOOG_RESOURCE_ID'];
+	}
+	if(isset($_SERVER['HTTP_X_GOOG_RESOURCE_STATE'])) {
+		$data['x_goog_resource_state'] = $_SERVER['HTTP_X_GOOG_RESOURCE_STATE'];
+	}
+	if(isset($_SERVER['HTTP_X_GOOG_RESOURCE_URI'])) {
+		$data['x_goog_resource_uri'] = $_SERVER['HTTP_X_GOOG_RESOURCE_URI'];
+	}
+	if(isset($_SERVER['HTTP_X_GOOG_CHANNEL_EXPIRATION'])) {
+		$data['x_goog_channel_expiration'] = $_SERVER['HTTP_X_GOOG_CHANNEL_EXPIRATION'];
+	}
+	if(isset($_SERVER['HTTP_X_GOOG_CHANNEL_TOKEN'])) {
+		$data['x_goog_channel_token'] = $_SERVER['HTTP_X_GOOG_CHANNEL_TOKEN'];
+	}
 
-if(isset($_SERVER['HTTP_X_GOOG_CHANNEL_ID'])) {
-	$data['x_goog_channel_id'] = $_SERVER['HTTP_X_GOOG_CHANNEL_ID'];
-	$unused_elseifs++;
-} elseif(isset($headers['X-Goog-Channel-ID'])) {
-	$data['x_goog_channel_id'] = $headers['X-Goog-Channel-ID'];
-}
-if(isset($_SERVER['HTTP_X_GOOG_MESSAGE_NUMBER'])) {
-	$data['x_goog_message_number'] = $_SERVER['HTTP_X_GOOG_MESSAGE_NUMBER'];
-	$unused_elseifs++;
-} elseif(isset($headers['X-Goog-Message-Number'])) {
-	$data['x_goog_message_number'] = $headers['X-Goog-Message-Number'];
-}
-if(isset($_SERVER['HTTP_X_GOOG_RESOURCE_ID'])) {
-	$data['x_goog_resource_id'] = $_SERVER['HTTP_X_GOOG_RESOURCE_ID'];
-	$unused_elseifs++;
-} elseif(isset($headers['X-Goog-Resource-ID'])) {
-	$data['x_goog_resource_id'] = $headers['X-Goog-Resource-ID'];
-}
-if(isset($_SERVER['HTTP_X_GOOG_RESOURCE_STATE'])) {
-	$data['x_goog_resource_state'] = $_SERVER['HTTP_X_GOOG_RESOURCE_STATE'];
-	$unused_elseifs++;
-} elseif(isset($headers['X-Goog-Resource-State'])) {
-	$data['x_goog_resource_state'] = $headers['X-Goog-Resource-State'];
-}
-if(isset($_SERVER['HTTP_X_GOOG_RESOURCE_URI'])) {
-	$data['x_goog_resource_uri'] = $_SERVER['HTTP_X_GOOG_RESOURCE_URI'];
-	$unused_elseifs++;
-} elseif(isset($headers['X-Goog-Resource-URI'])) {
-	$data['x_goog_resource_uri'] = $headers['X-Goog-Resource-URI'];
-}
-if(isset($_SERVER['HTTP_X_GOOG_CHANNEL_EXPIRATION'])) {
-	$data['x_goog_channel_expiration'] = $_SERVER['HTTP_X_GOOG_CHANNEL_EXPIRATION'];
-	$unused_elseifs++;
-} elseif(isset($headers['X-Goog-Channel-Expiration'])) {
-	$data['x_goog_channel_expiration'] = $headers['X-Goog-Channel-Expiration'];
-}
-if(isset($_SERVER['HTTP_X_GOOG_CHANNEL_TOKEN'])) {
-	$data['x_goog_channel_token'] = $_SERVER['HTTP_X_GOOG_CHANNEL_TOKEN'];
-	$unused_elseifs++;
-} elseif(isset($headers['X-Goog-Channel-Token'])) {
-	$data['x_goog_channel_token'] = $headers['X-Goog-Channel-Token'];
-}
+	$str = '';
+	foreach($data as $key=>$val) {
+		$str .= "$key: $val\n";
+	}
+	$str .= "------------------------------------END-REQUEST-----------------------------------------\n\n";
 
-$str = '';
-foreach($data as $key=>$val) {
-	$str .= "$key: $val\n";
+	$file = 'webhook.thelog';
+	$handler = fopen($file, 'a') or die("can't open file");
+	fwrite($handler, $str);
+	fclose($handler);
 }
-$str .= "Unused Elseif's: $unused_elseifs\n";
-$str .= "------------------------------------END-REQUEST-----------------------------------------\n\n";
-
-$file = 'webhook.thelog';
-$handler = fopen($file, 'a') or die("can't open file");
-fwrite($handler, $str);
-fclose($handler);
 
 header("HTTP/1.1 200 OK");
 // header('HTTP/1.1 500 Internal Server Error');
