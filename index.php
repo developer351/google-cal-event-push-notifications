05ce4cd3fca037784d75f260e39eb2a10a954ed9
Brice Bentler <server4001@gmail.com>
2014-07-28 11:19:12 -0700

    Changes URL back to original one

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (isset($_GET['code'])) {
   	$access_json = $client->getAccessToken();
   	$access_object = json_decode($access_json);
   	$access_token = $access_object->access_token;
-	$url = "https://www.googleapis.com/calendar/v3/calendars/u9bg7l9pd9h1bfhd948neoua90/events/watch";
+	$url = "https://www.googleapis.com/calendar/v3/calendars/primary/events/watch";
 	$random_hex = md5(uniqid(mt_rand(), true));
 	$token = md5(uniqid(mt_rand(), true)) . md5(uniqid(mt_rand(), true));
 	/* setup the POST parameters */
