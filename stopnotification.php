788f2f20facaf1bde8200f75b20df78bc0585026
Brice Bentler <server4001@gmail.com>
2014-07-29 20:58:32 -0700

    Swaps out stop notification cURL for Google PHP SDK

--- stopnotification.php
+++ stopnotification.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'Google/Client.php';
+require_once 'Google/Service/Calendar.php';
+
 $client_id = '867444719707-cd6p1jd0pgjhgqp6mk073k6hvs2dtulp.apps.googleusercontent.com';
 $client_secret = 'emJpbLliLu5whjClOBRCHttZ';
 $redirect_uri = 'http://www.applecrateseo.com/googlecalpush/stopnotification.php';
@@ -11,36 +13,40 @@ $client->setScopes('https://www.googleapis.com/auth/calendar');
 
 if (isset($_GET['code'])) {
 	$client->authenticate($_GET['code']);
-  	$access_json = $client->getAccessToken();
-  	$access_object = json_decode($access_json);
-  	$access_token = $access_object->access_token;
-	$url = "https://www.googleapis.com/calendar/v3/channels/stop";
-	/* setup the POST parameters */
-	$fields = json_encode(array(
-	    'id'      => "e09cbc80745b77ef4d0d3a191e57cdc6",
-	    'resourceId'   => 'o8NNPk5X3IO__w-CeO7U6WgkvWY'
-    ));
 
-	/* setup POST headers */
-	$headers[] = 'Content-Type: application/json';
-	$headers[] = 'Authorization: Bearer ' . $access_token;
+	$service = new Google_Service_Calendar($client);
+	$channel = new Google_Service_Calendar_Channel($client);
+	$channel->setId('e09cbc80745b77ef4d0d3a191e57cdc6');
+	$channel->setResourceId('o8NNPk5X3IO__w-CeO7U6WgkvWY');
 
-	/* send POST request */
-	$channel = curl_init();
-	curl_setopt($channel, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($channel, CURLOPT_URL, $url);
-	curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($channel, CURLOPT_POST, true);
-	curl_setopt($channel, CURLOPT_POSTFIELDS, $fields);
-	curl_setopt($channel, CURLOPT_CONNECTTIMEOUT, 2);
-	curl_setopt($channel, CURLOPT_TIMEOUT, 3);
-	$response = curl_exec($channel);
-	curl_close($channel);
+	$channelStop = $service->channels->stop($channel);
+	var_dump($channelStop);die();
+  	
+	// $url = "https://www.googleapis.com/calendar/v3/channels/stop";
+	// /* setup the POST parameters */
+	// $fields = json_encode(array(
+	//     'id'      => "e09cbc80745b77ef4d0d3a191e57cdc6",
+	//     'resourceId'   => 'o8NNPk5X3IO__w-CeO7U6WgkvWY'
+ //    ));
 
-	var_dump($response);
-	echo '<br><br>';
-	var_dump($access_token);
-	echo '<br><br>';
+	// /* setup POST headers */
+	// $headers[] = 'Content-Type: application/json';
+	// $headers[] = 'Authorization: Bearer ' . $access_token;
+
+	// /* send POST request */
+	// $channel = curl_init();
+	// curl_setopt($channel, CURLOPT_HTTPHEADER, $headers);
+	// curl_setopt($channel, CURLOPT_URL, $url);
+	// curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
+	// curl_setopt($channel, CURLOPT_POST, true);
+	// curl_setopt($channel, CURLOPT_POSTFIELDS, $fields);
+	// curl_setopt($channel, CURLOPT_CONNECTTIMEOUT, 2);
+	// curl_setopt($channel, CURLOPT_TIMEOUT, 3);
+	// $response = curl_exec($channel);
+	// curl_close($channel);
+
+	// var_dump($response);
+	// echo '<br><br>';
 } else {
 	$authUrl = $client->createAuthUrl();
 }
